5a20dc065970b3e4d785bea0543a05c52476fdb6
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2010-08-25 13:12:51 +0000

    bugfix: rosen derivative & hessian now active code
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@415 968178ea-60bd-409e-af13-df8a517b6005

--- models/dejong.py
+++ models/dejong.py
@@ -17,6 +17,7 @@ from abstract_model import AbstractFunction
 
 from numpy import sum as numpysum
 from numpy import asarray, transpose
+from numpy import zeros_like, diag, zeros, atleast_1d
 from math import floor
 import random
 from math import pow
@@ -33,15 +34,58 @@ A modified second De Jong function, Equation (18) of [2]"""
         """evaluates n-dimensional Rosenbrock function for a list of coeffs
 
 minimum is f(x)=0.0 at xi=1.0"""
-        #ensure that there are 2 coefficients
-        x = [1]*2
+        x = [1]*2 # ensure that there are 2 coefficients
         x[:len(coeffs)]=coeffs
         x = asarray(x) #XXX: must be a numpy.array
-        return numpysum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+        return numpysum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)#,axis=0)
 
-#   def forward(self,pts):
-#       """n-dimensional Rosenbrock; returns f(xi,yi,...) for pts=(x,y,...)"""
-#       return AbstractFunction.forward(self,pts)
+    #def forward(self,pts):
+    #    """n-dimensional Rosenbrock; returns f(xi,yi,...) for pts=(x,y,...)"""
+    #    return AbstractFunction.forward(self,pts)
+
+    def derivative(self,coeffs):
+        """evaluates n-dimensional Rosenbrock derivative for a list of coeffs
+
+minimum is f'(x)=[0.0]*n at x=[1.0]*n;  x must have len >= 2"""
+        l = len(coeffs)
+        x = [0]*l #XXX: ensure that there are 2 coefficients ?
+        x[:l]=coeffs
+        x = asarray(x) #XXX: must be a numpy.array
+        xm = x[1:-1]
+        xm_m1 = x[:-2]
+        xm_p1 = x[2:]
+        der = zeros_like(x)
+        der[1:-1] = 200*(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
+        der[0] = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
+        der[-1] = 200*(x[-1]-x[-2]**2)
+        return list(der)
+
+    def hessian(self, coeffs):
+        """evaluates n-dimensional Rosenbrock hessian for the given coeffs
+
+coeffs must have len >= 2"""
+        x = atleast_1d(coeffs)
+        H = diag(-400*x[:-1],1) - diag(400*x[:-1],-1)
+        diagonal = zeros(len(x), dtype=x.dtype)
+        diagonal[0] = 1200*x[0]-400*x[1]+2
+        diagonal[-1] = 200
+        diagonal[1:-1] = 202 + 1200*x[1:-1]**2 - 400*x[2:]
+        H = H + diag(diagonal)
+        return H
+
+    def hessian_product(self, coeffs, p):
+        """evaluates n-dimensional Rosenbrock hessian product for the given coeffs
+
+both p and coeffs must have len >= 2"""
+        #XXX: not well-tested
+        p = atleast_1d(p)
+        x = atleast_1d(coeffs)
+        Hp = zeros(len(x), dtype=x.dtype)
+        Hp[0] = (1200*x[0]**2 - 400*x[1] + 2)*p[0] - 400*x[0]*p[1]
+        Hp[1:-1] = -400*x[:-2]*p[:-2]+(202+1200*x[1:-1]**2-400*x[2:])*p[1:-1] \
+                   -400*x[1:-1]*p[2:]
+        Hp[-1] = -400*x[-2]*p[-2] + 200*p[-1]
+        return Hp
 
     pass
  
