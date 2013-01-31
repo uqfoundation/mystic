e8678f278ac633e901b27f6a4e8535eb6b5c58cc
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2013-01-31 04:47:57 +0000

    added light testing of scipy_optimizers to test_solver_state
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@650 968178ea-60bd-409e-af13-df8a517b6005

--- tests/test_solver_state.py
+++ tests/test_solver_state.py
@@ -1,8 +1,52 @@
-from mystic.differential_evolution import DifferentialEvolutionSolver
+from mystic.solvers import DifferentialEvolutionSolver
+from mystic.solvers import NelderMeadSimplexSolver, PowellDirectionalSolver
 from mystic.termination import VTR
 from mystic.models import rosen
 from mystic.solvers import LoadSolver
 import os
+
+solver = PowellDirectionalSolver(3)
+solver.SetRandomInitialPoints([0.,0.,0.],[10.,10.,10.])
+term = VTR()
+solver.Solve(rosen, term)
+x = solver.bestSolution
+y = solver.bestEnergy  
+assert solver._state == None
+assert LoadSolver(solver._state) == None
+
+solver = PowellDirectionalSolver(3)
+solver.SetRandomInitialPoints([0.,0.,0.],[10.,10.,10.])
+term = VTR()
+tmpfile = 'mysolver.pkl'
+solver.SetSaveFrequency(10, tmpfile)
+solver.Solve(rosen, term)
+x = solver.bestSolution
+y = solver.bestEnergy  
+_solver = LoadSolver(tmpfile)
+os.remove(tmpfile)
+assert all(x == _solver.bestSolution)
+assert y == _solver.bestEnergy  
+
+solver = NelderMeadSimplexSolver(3)
+solver.SetRandomInitialPoints([0.,0.,0.],[10.,10.,10.])
+term = VTR()
+solver.Solve(rosen, term)
+x = solver.bestSolution
+y = solver.bestEnergy  
+assert solver._state == None
+assert LoadSolver(solver._state) == None
+
+solver = NelderMeadSimplexSolver(3)
+solver.SetRandomInitialPoints([0.,0.,0.],[10.,10.,10.])
+term = VTR()
+solver.SetSaveFrequency(10, tmpfile)
+solver.Solve(rosen, term)
+x = solver.bestSolution
+y = solver.bestEnergy  
+_solver = LoadSolver(tmpfile)
+os.remove(tmpfile)
+assert all(x == _solver.bestSolution)
+assert y == _solver.bestEnergy  
 
 solver = DifferentialEvolutionSolver(3,40)
 solver.SetRandomInitialPoints([0.,0.,0.],[10.,10.,10.])
@@ -16,7 +60,6 @@ assert LoadSolver(solver._state) == None
 solver = DifferentialEvolutionSolver(3,40)
 solver.SetRandomInitialPoints([0.,0.,0.],[10.,10.,10.])
 term = VTR()
-tmpfile = 'mysolver.pkl'
 solver.SetSaveFrequency(10, tmpfile)
 solver.Solve(rosen, term)
 x = solver.bestSolution
