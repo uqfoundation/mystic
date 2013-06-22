54033b438900e17d8e3e852222dc25c981cdb7e7
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2013-06-22 18:15:56 +0000

    add model_helper, forgot to commit it previously
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@693 968178ea-60bd-409e-af13-df8a517b6005

---
+++ models/_model_helper.py
@@ -0,0 +1,26 @@
+
+chebyshev2coeffs = [2., 0., -1.]
+chebyshev4coeffs = [8., 0., -8., 0., 1.]
+chebyshev6coeffs = [32., 0., -48., 0., 18., 0., -1.]
+chebyshev8coeffs = [128., 0., -256., 0., 160., 0., -32., 0., 1.]
+chebyshev16coeffs = [32768., 0., -131072., 0., 212992., 0., -180224., 0., 84480., 0., -21504., 0., 2688., 0., -128., 0., 1]
+
+def chebyshev(trial, target, M=61):
+    from mystic.math import polyeval
+    result=0.0
+    x=-1.0
+    dx = 2.0 / (M-1)
+    for i in range(M):
+        px = polyeval(trial, x)
+        if px<-1 or px>1:
+            result += (1 - px) * (1 - px)
+        x += dx
+
+    px = polyeval(trial, 1.2) - polyeval(target, 1.2)
+    if px<0: result += px*px
+
+    px = polyeval(trial, -1.2) - polyeval(target, -1.2)
+    if px<0: result += px*px
+
+    return result
+
