876190a3fbcba0501c9a8199b457278d9e0ee759
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2015-08-03 13:41:37 +0000

    added basic cvxopt lp and qp example
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@811 968178ea-60bd-409e-af13-df8a517b6005

---
+++ examples2/cvxqp.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# Problem definition:
+# Example in reference documentation for cvxopt
+# http://cvxopt.org/examples/tutorial/qp.html
+# 
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 1997-2015 California Institute of Technology.
+# License: 3-clause BSD.  The full license text is available at:
+#  - http://trac.mystic.cacr.caltech.edu/project/mystic/browser/mystic/LICENSE
+"""
+    Minimize: f = 2*x[0]**2 + x[1]**2 + x[0]*x[1] + x[0] + x[1]
+
+    Subject to:     x[0] >= 0
+                           x[1] >= 0
+                    x[0] + x[1] == 1
+"""
+
+def objective(x):
+  x0,x1 = x
+  return 2*x0**2 + x1**2 + x0*x1 + x0 + x1
+
+equations = """
+x0 + x1 - 1.0 == 0.0
+"""
+bounds = [(0.0, None),(0.0, None)]
+
+# with penalty='penalty' applied, solution is:
+xs = [0.25, 0.75]
+ys = 1.875
+
+from mystic.symbolic import generate_conditions, generate_penalty
+pf = generate_penalty(generate_conditions(equations), k=1e4)
+
+
+if __name__ == '__main__':
+
+  from mystic.solvers import diffev2, fmin_powell
+  from mystic.math import almostEqual
+
+  result = diffev2(objective, x0=bounds, bounds=bounds, penalty=pf, npop=40, disp=False, full_output=True)
+  assert almostEqual(result[0], xs, rel=2e-2)
+  assert almostEqual(result[1], ys, rel=2e-2)
+
+  result = fmin_powell(objective, x0=[0.0,0.0], bounds=bounds, penalty=pf, disp=False, full_output=True)
+  assert almostEqual(result[0], xs, rel=2e-2)
+  assert almostEqual(result[1], ys, rel=2e-2)
+
+
+# EOF
---
+++ examples2/cvxqp_alt2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# Problem definition:
+# Example in reference documentation for cvxopt
+# http://cvxopt.org/examples/tutorial/qp.html
+# 
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 1997-2015 California Institute of Technology.
+# License: 3-clause BSD.  The full license text is available at:
+#  - http://trac.mystic.cacr.caltech.edu/project/mystic/browser/mystic/LICENSE
+
+from cvxqp import objective, bounds, xs, ys
+
+from mystic.math.measures import normalize
+
+def constraint(x): # impose exactly
+    return normalize(x)
+
+
+if __name__ == '__main__':
+
+  from mystic.solvers import diffev2, fmin_powell
+  from mystic.math import almostEqual
+
+  result = diffev2(objective, x0=bounds, bounds=bounds, constraints=constraint, npop=40, disp=False, full_output=True)
+  assert almostEqual(result[0], xs, rel=1e-5)
+  assert almostEqual(result[1], ys, rel=1e-5)
+
+
+# EOF
