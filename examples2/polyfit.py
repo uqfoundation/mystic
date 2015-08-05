e267350811893eff85f63ad3182b2c46e9e8178d
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2015-08-05 01:38:33 +0000

    add polynomial fitting example
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@813 968178ea-60bd-409e-af13-df8a517b6005

---
+++ examples2/polyfit.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 1997-2015 California Institute of Technology.
+# License: 3-clause BSD.  The full license text is available at:
+#  - http://trac.mystic.cacr.caltech.edu/project/mystic/browser/mystic/LICENSE
+"""
+    Fit linear and quadratic polynomial to noisy data
+
+    where:
+               0 >= x >= 4
+               y = y0 + yn
+               y0 = 1.5 * exp(-0.2 * x) + 0.3
+               yn = 0.1 * Normal(0,1)
+"""
+from numpy import polyfit, poly1d, linspace, exp
+from numpy.random import normal
+from mystic.math import polyeval
+from mystic import reduced
+
+# Create clean data.
+x = linspace(0, 4.0, 100)
+y0 = 1.5 * exp(-0.2 * x) + 0.3
+
+# Add a bit of noise.
+noise = 0.1 * normal(size=100) 
+y = y0 + noise
+
+@reduced(lambda x,y: abs(x)+abs(y))
+def objective(coeffs, x, y):
+    return polyeval(coeffs, x) - y
+
+bounds  = [(None, None), (None, None)]
+bounds_ = [(None, None), (None, None), (None, None)]
+
+args = (x, y)
+
+# 'solution' is:
+xs  = polyfit(x, y, 1) 
+xs_ = polyfit(x, y, 2) 
+ys  = objective(xs, x, y)
+ys_ = objective(xs_, x, y)
+
+
+if __name__ == '__main__':
+
+  from mystic.solvers import diffev2, fmin_powell
+  from mystic.math import almostEqual
+# from mystic.monitors import VerboseMonitor
+# mon = VerboseMonitor(10)
+
+  result = diffev2(objective, args=args, x0=bounds, bounds=bounds, npop=40, ftol=1e-8, gtol=100, disp=False, full_output=True)#, itermon=mon)
+# print result[0], xs
+  assert almostEqual(result[0], xs, rel=1e-1)
+  assert almostEqual(result[1], ys, rel=1e-1)
+
+  result = fmin_powell(objective, args=args, x0=[0.0,0.0], bounds=bounds, disp=False, full_output=True)
+# print result[0], xs
+  assert almostEqual(result[0], xs, rel=1e-1)
+  assert almostEqual(result[1], ys, rel=1e-1)
+
+# mon = VerboseMonitor(10)
+  result = diffev2(objective, args=args, x0=bounds_, bounds=bounds_, npop=40, ftol=1e-8, gtol=100, disp=False, full_output=True)#, itermon=mon)
+# print result[0], xs_
+  assert almostEqual(result[0], xs_, tol=1e-1)
+  assert almostEqual(result[1], ys_, rel=1e-1)
+
+  result = fmin_powell(objective, args=args, x0=[0.0,0.0,0.0], bounds=bounds_, disp=False, full_output=True)
+# print result[0], xs_
+  assert almostEqual(result[0], xs_, tol=1e-1)
+  assert almostEqual(result[1], ys_, rel=1e-1)
+
+
+# EOF
