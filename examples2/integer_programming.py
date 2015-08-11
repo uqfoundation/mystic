15251f1fd642753d013989a898646d446e20f1ca
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2015-08-11 12:41:23 +0000

    added ortools integer_programming example
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@818 968178ea-60bd-409e-af13-df8a517b6005

---
+++ examples2/integer_programming.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# Problem definition:
+# Example in google/or-tools
+# https://github.com/google/or-tools/blob/master/examples/python/integer_programming.py
+# with Copyright 2010-2013 Google
+# and disclamer as stated at the above reference link.
+# 
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 1997-2015 California Institute of Technology.
+# License: 3-clause BSD.  The full license text is available at:
+#  - http://trac.mystic.cacr.caltech.edu/project/mystic/browser/mystic/LICENSE
+"""
+  Integer programming example
+
+  Minimize:
+            f(x) = x0 + 2*x1
+
+  Where: 
+            3*x0 + 2*x1 >= 17
+            x0,x1 are integers
+"""
+
+def objective(x):
+    return x[0] + 2*x[1]
+
+bounds = [(0,None)]*2
+
+# with penalty='penalty' applied, solution is:
+xs = [6., 0.]
+ys = 6.0
+
+# constraints
+equations = """
+3.*x0 + 2.*x1 - 17. >= 0.0
+"""
+
+from mystic.symbolic import generate_penalty, generate_conditions
+pf = generate_penalty(generate_conditions(equations))
+
+from numpy import round as npround
+
+if __name__ == '__main__':
+
+    from mystic.solvers import diffev2
+    from mystic.math import almostEqual
+
+    result = diffev2(objective, x0=bounds, bounds=bounds, penalty=pf, constraints=npround, npop=20, gtol=50, disp=True, full_output=True)
+
+    print result[0]
+    assert almostEqual(result[0], xs, tol=1e-8) #XXX: fails b/c rel & zero?
+    assert almostEqual(result[1], ys, tol=1e-4)
+
+
+# EOF
---
+++ examples2/integer_programming_alt.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Problem definition:
+# Example in google/or-tools
+# https://github.com/google/or-tools/blob/master/examples/python/integer_programming.py
+# with Copyright 2010-2013 Google
+# and disclamer as stated at the above reference link.
+# 
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 1997-2015 California Institute of Technology.
+# License: 3-clause BSD.  The full license text is available at:
+#  - http://trac.mystic.cacr.caltech.edu/project/mystic/browser/mystic/LICENSE
+
+from integer_programming import objective, bounds, xs, ys
+# bounds = [(0,11)]*2  #XXX: MOD = range(11) instead of LARGE
+
+from mystic.penalty import quadratic_inequality, quadratic_equality
+from mystic.constraints import as_constraint, discrete
+
+def penalty1(x): # <= 0.0
+    return -3*x[0] - 2*x[1] + 17.0
+
+def penalty2(x): # == 0.0
+    from numpy import abs, round
+    return abs(x - round(x)).sum() # penalize when not an 'int'
+
+#@quadratic_equality(penalty2)
+@quadratic_inequality(penalty1)
+def penalty(x):
+    return 0.0
+
+solver = as_constraint(penalty)
+#solver = discrete(range(11))(solver)  #XXX: MOD = range(11) instead of LARGE
+#FIXME: constrain to 'int' with discrete is very fragile!  required #MODs
+
+def constraint(x):
+    from numpy import round
+    return round(solver(x))
+
+from numpy import round as npround
+
+if __name__ == '__main__':
+
+    from mystic.solvers import diffev2
+    from mystic.math import almostEqual
+
+    result = diffev2(objective, x0=bounds, bounds=bounds, penalty=penalty, constraints=npround, npop=20, gtol=50, disp=True, full_output=True)
+
+    print result[0]
+    assert almostEqual(result[0], xs, tol=1e-8) #XXX: fails b/c rel & zero?
+    assert almostEqual(result[1], ys, tol=1e-4)
+
+
+# EOF
