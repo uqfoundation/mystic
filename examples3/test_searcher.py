678c9046fb26a38164e9d63281093adb0933b6d2
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2015-11-22 18:10:32 +0000

    added initial global search object and accompanying example
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@842 968178ea-60bd-409e-af13-df8a517b6005

---
+++ examples3/test_searcher.py
@@ -0,0 +1,208 @@
+"""
+example of using a global searcher for interpolation
+"""
+
+from mystic.search import Searcher
+#import time
+
+
+if __name__ == '__main__':
+#   start = time.time()
+    # if available, use a multiprocessing worker pool
+    try: #NOTE: ThreadPool and ParallelPool fail
+        from pathos.helpers import freeze_support
+        freeze_support()
+        from pathos.pools import ProcessPool as Pool
+    except ImportError:
+        from mystic.pools import SerialPool as Pool
+
+    # tools
+    from mystic.termination import VTR, ChangeOverGeneration as COG
+    from mystic.termination import NormalizedChangeOverGeneration as NCOG
+    from mystic.monitors import LoggingMonitor, VerboseMonitor, Monitor
+    from klepto.archives import dir_archive
+
+    stop = NCOG(1e-4)
+    disp = False # print optimization summary
+    stepmon = False # use LoggingMonitor
+    archive = False # save an archive
+
+    traj = not stepmon # save all trajectories internally, if no logs
+
+    # cost function
+    from mystic.models import griewangk as model
+    ndim = 2  # model dimensionality
+    bounds = ndim * [(-9.5,9.5)] # griewangk
+
+    # the ensemble solvers
+    from mystic.solvers import BuckshotSolver, LatticeSolver
+    # the local solvers
+    from mystic.solvers import PowellDirectionalSolver
+
+    sprayer = BuckshotSolver
+    seeker = PowellDirectionalSolver
+    npts = 25 # number of solvers
+    _map = Pool().map
+    retry = 1 # max consectutive iteration retries without a cache 'miss'
+    tol = 8   # rounding precision
+    mem = 1   # cache rounding precision
+
+    #CUTE: 'configure' monitor and archive if they are desired
+    if stepmon: stepmon = LoggingMonitor(1) # montor for all runs
+    else: stepmon = None
+    if archive:
+        ar_name = '__%s_%sD_cache__' % (model.im_class.__name__,ndim)
+        archive = dir_archive(ar_name, serialized=True, cached=False)
+    else: archive = None
+
+    searcher = Searcher(npts, retry, tol, mem, _map, archive, sprayer, seeker)
+    searcher.Verbose(disp)
+    searcher.UseTrajectories(traj)
+
+    searcher.Reset(archive, inv=False)
+    searcher.Search(model, bounds, stop=stop, monitor=stepmon)
+    searcher._summarize()
+
+    ##### extract results #####
+    xyz = searcher.Samples()
+
+    ##### invert the model, and get the maxima #####
+    imodel = lambda *args, **kwds: -model(*args, **kwds)
+
+    #CUTE: 'configure' monitor and archive if they are desired
+    if stepmon not in (None, False):
+        itermon = LoggingMonitor(1, filename='inv.txt') #XXX: log.txt?
+    else: itermon = None
+    if archive not in (None, False):
+        ar_name = '__%s_%sD_invcache__' % (model.im_class.__name__,ndim)
+        archive = dir_archive(ar_name, serialized=True, cached=False)
+    else: archive = None
+
+    searcher.Reset(archive, inv=True)
+    searcher.Search(imodel, bounds, stop=stop, monitor=itermon)
+    searcher._summarize()
+
+    ##### extract results #####
+    import numpy as np
+    xyz = np.hstack((xyz, searcher.Samples()))
+#   print "TOOK: %s" % (time.time() - start)
+
+    ########## interpolate ##########
+    from scipy.interpolate import Rbf
+
+    #############
+    shift = 0
+    scale = 0
+    N = 1000.
+    M = 200
+    args = {
+    'smooth': 0,
+    'function': 'thin_plate',
+    }
+    #############
+
+    # get params (x) and cost (z)
+    x, z = xyz.T[:,:-1], xyz.T[:,-1]
+
+    #HACK: remove any duplicate points by adding noise
+    _x = x + np.random.normal(scale=1e-8, size=x.shape)
+
+    if len(z) > N:
+        N = max(int(round(len(z)/float(N))),1)
+        print "for speed, sampling {} down to {}".format(len(z),len(z)/N)
+        x, _x, z = x[::N], _x[::N], z[::N]
+
+    f = Rbf(*np.vstack((_x.T, z)), **args)
+    f.__doc__ = model.__doc__
+    # convert to 'model' format (i.e. takes a parameter vector)
+    _model = lambda x: f(*x).tolist()
+    _model.__doc__ = f.__doc__
+
+    mz = np.argmin(z)
+    print "min: {}; min@f: {}".format(z[mz], f(*x[mz]))
+    mz = np.argmax(z)
+    print "max: {}; max@f: {}".format(z[mz], f(*x[mz]))
+
+#   print "TOOK: %s" % (time.time() - start)
+
+    # plot
+    #############
+    # specify 2-of-N dim (with bounds) and (N-2) with values
+    axes = (0,1)  # axes to plot               (specified by user)
+    vals = ()     # values for remaining param (specified by user)
+    #############
+
+    from mpl_toolkits.mplot3d import axes3d
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+
+    figure = plt.figure()
+    kwds = {'projection':'3d'}
+    ax = figure.gca(**kwds)
+    ax.autoscale(tight=True)
+
+    # build a list of fixed values (default zero) and override with user input
+    ix = [i for i in range(len(x.T)) if i not in axes]
+   #fix = np.zeros(len(ix))
+    fix = enumerate(x[np.argmin(z)])
+    fix = np.array(tuple(j for (i,j) in fix if i not in axes))
+    fix[:len(vals)] = vals
+
+    # build a grid of points, one for each param, and apply fixed values
+    grid = np.ones((len(x.T),M,M))
+    grid[ix] = fix[:,None,None]
+    del ix, fix
+
+    # build the sub-surface of surrogate(x) to display, and apply to the grid
+    axes = list(axes)
+    xy = x.T[axes]
+    S = complex('{}j'.format(M))
+    grid[axes] = np.mgrid[xy[0].min():xy[0].max():S, xy[1].min():xy[1].max():S]
+    del xy
+
+    # evaluate the surrogate on the sub-surface
+    z_ = f(*grid)
+
+    # scaling used by model plotter
+    if scale:
+        if shift:
+            z_ = np.asarray(z_)+shift
+            z = np.asarray(z)+shift
+        z_ = np.log(4*np.asarray(z_)*scale+1)+2
+        z = np.log(4*np.asarray(z)*scale+1)+2
+
+    # plot the surface and points
+    density = 9
+    d = max(11 - density, 1)
+    x_ = grid[axes[0]]
+    y_ = grid[axes[-1]]
+    ax.plot_wireframe(x_, y_, z_, rstride=d, cstride=d)
+    #ax.plot_surface(x_, y_, z_, rstride=d, cstride=d, cmap=cm.jet, linewidth=0, antialiased=False)
+    x_ = x.T[axes[0]]
+    y_ = x.T[axes[-1]]
+    ax.plot(x_, y_, z, 'ko', linewidth=2, markersize=4)
+
+    plt.show()
+#   figure.savefig('griewangk.png')
+
+    """
+    try:
+        from klepto.archives import file_archive
+        archive = file_archive('models.pkl', serialized=True, cached=False)
+        archive[model.im_class.__name__.lower()] = f
+    except Exception:
+        print "serialization failed"
+    """
+
+    # some testing of interpolated model
+    data = np.asarray(z)
+#   actual = [model([xi,yi]) for (xi,yi) in zip(x,y)]
+    interp = f(*x.T)
+    print "sum diff squares"
+#   print "data and actual: %s" % np.sum((data - actual)**2)
+#   print "data and interp: %s" % np.sum((data - interp)**2)
+#   print "actual and interp: %s" % np.sum((actual - interp)**2)
+    print "actual and interp: %s" % np.sum((data - interp)**2)
+
+
+# EOF
---
+++ mystic/search.py
@@ -0,0 +1,230 @@
+"""
+a global searcher
+"""
+
+class Searcher(object):
+   #searcher has:
+   #    sprayer - an ensemble algorithm
+   #    archive - a sampled point archive(s)
+   #    retry - max consectutive retries w/o an archive 'miss'
+   #    tol - minima comparator rounding precision
+   #    _allSolvers - collection of sprayers (for all solver trajectories)
+   #
+   #searcher (or archive) has:
+   #    memtol - memoization rounding precision
+   #
+   #searcher (or sprayer) has:
+   #    seeker - an optimiztion algorithm
+   #    map - an enselble map
+   #    traj - trajectory configuration (bool)
+   #    disp - verbosity configuration (bool)
+   #    npts - number of solvers
+   #
+   #searcher can:
+   #    _memoize - apply caching archive to sprayer
+   #    _search - perform a single search iteration (the search algorithm ?)
+   #    Search - keep performing _search until retry condition is met
+   #    UseTrajectories - save all sprayers, thus save all trajectories
+   #    Reset - clear caching archive and saved sprayers (thus trajectories)
+   #    Trajectories - fetch (step, param, cost) for all solver trajectories
+   #    Samples - all sampled points as a n-D array (output is array[-1])
+   #    Values - fetch values (model(x)) for sampled points on surface (model)
+   #    Coordinates - fetch inputs (x) for sampled points on surface (model)
+   #    Minima - fetch {x: model(x)} for all x that minimize model
+   #    _summarize - print diagnostics on the value/quantity of solved values
+   #
+   # searcher (or sprayer) can:
+   #    _print - print bestSolution and bestEnergy for each sprayer
+   #    _configure (model, bounds, stop, monitor) - configure sprayer
+   #    _solve - spray multiple seekers
+
+    def __init__(self, npts=4, retry=1, tol=8, memtol=1, map=None,
+              archive=None, sprayer=None, seeker=None, traj=False, disp=False):
+
+        #XXX: better not to use klepto as default? just dict and false cache?
+        from klepto.archives import dict_archive as _archive
+        from mystic.solvers import BuckshotSolver
+        from mystic.solvers import PowellDirectionalSolver
+        from mystic.pools import SerialPool as Pool
+        from __builtin__ import map as _map
+        self.archive = _archive(cached=False) if archive is None else archive
+        self.sprayer = BuckshotSolver if sprayer is None else sprayer
+        self.seeker = PowellDirectionalSolver if seeker is None else seeker
+        self.map = Pool().map if map in (None, _map) else map 
+        self.traj = traj
+        self.disp = disp
+
+        self.npts = npts # number of solvers
+        self.retry = retry   # max consectutive retries w/o a cache 'miss'
+        self.tol = tol       # rounding precision
+        self.memtol = memtol # memoization rounding precision
+        self._allSolvers = []
+        self._inv = False    # self-awareness: am I inverted (maximizing)?
+        return
+
+    def _print(self, solver, tol=8):
+        l = -1 if self._inv else 1
+        for _solver in solver._allSolvers:
+            bestSol = tuple(round(s, tol) for s in _solver.bestSolution)
+            bestRes = round(_solver.bestEnergy, tol)
+            print (bestSol, l*bestRes)
+
+    def _memoize(self, solver, tol=1):
+        from klepto import inf_cache
+        from klepto.keymaps import keymap
+
+        km = keymap()
+        ca = inf_cache(tol=tol, ignore=('**','out'),
+                       cache=self.archive, keymap=km)
+
+        @ca
+        def memo(*args, **kwds):
+            return kwds['out']
+
+        l = -1 if self._inv else 1
+        for _solver in solver._allSolvers:
+            bestSol = tuple(_solver.bestSolution)
+            bestRes = float(_solver.bestEnergy)
+            memo(*bestSol, out=l*bestRes)
+        return memo
+
+    def _configure(self, model, bounds, stop=None, monitor=None):
+        from mystic.monitors import Monitor
+        # configure monitor
+        monitor = Monitor() if monitor is None else monitor
+
+        # get dimensions
+        ndim = len(bounds)
+        _min, _max = zip(*bounds)
+
+        # configure ensemble solver
+        self.solver = self.sprayer(ndim, self.npts)
+        self.solver.SetNestedSolver(self.seeker)
+        self.solver.SetMapper(self.map)
+        self.solver.SetObjective(model)
+        if stop: self.solver.SetTermination(stop)
+        self.solver.SetGenerationMonitor(monitor) #NOTE: may be xy,-z
+        self.solver.SetStrictRanges(min=_min, max=_max)
+        return
+
+    def _solve(self, id=None, disp=None):
+        from copy import deepcopy as _copy
+        solver = _copy(self.solver) #FIXME: python2.6
+        # configure solver
+        solver.id = id
+        model = solver._cost[1] #FIXME: HACK b/c Solve(model) is required
+        # solve
+        disp = self.disp if disp is None else disp
+#       import time
+#       start = time.time()
+        solver.Solve(model, disp=disp)
+#       print "TOOK: %s" % (time.time() - start)
+        return solver
+
+    def _search(self, sid):
+        solver = self._solve(sid, self.disp)
+        if self.traj: self._allSolvers.append(solver)
+        sid += len(solver._allSolvers)
+#       self._print(solver, tol=self.tol)
+        info = self._memoize(solver, tol=self.memtol).info()
+        if self.disp: print info
+        size = info.size
+        return sid, size
+
+    def UseTrajectories(self, traj=True):
+        self.traj = bool(traj)
+        return
+
+    def Verbose(self, disp=True):
+        self.disp = bool(disp)
+        return
+
+    def Search(self, model, bounds, stop=None, monitor=None, traj=None, disp=None):
+        self.traj = self.traj if traj is None else traj
+        self.disp = self.disp if disp is None else disp
+        self._configure(model, bounds, stop, monitor)
+        count = 0 if self.retry else -1 #XXX: 'rerun' much shorter... unless clear
+        sid = 0  # keep track of which solver is which across multiple runs
+        while self.retry > count: # stop after retry consecutive no new results
+            _size = -1
+            size = osize = len(self.archive) #XXX: compare 'size' or 'len(vals)'?
+            while size > _size: # stop if no new results
+                _size = size
+                sid, size = self._search(sid) # uses self.traj and self.disp
+            if size == osize: count = count + 1
+            else: count = 0
+
+        #NOTE: traj & disp are sticky
+        return
+
+    def Reset(self, archive=None, inv=None):
+        if archive is None: self.archive.clear() #XXX: clear the archive?
+        self.archive = self.archive if archive is None else archive
+        [self._allSolvers.pop() for i in range(len(self._allSolvers))]
+        if inv is not None: self._inv = inv
+
+    def Values(self, unique=False):
+        vals = self.archive.itervalues()
+        new = set()
+        return [v for v in vals if v not in new and not new.add(v)] if unique else list(vals)
+
+    def Coordinates(self, unique=False):
+        keys = self.archive.iterkeys()
+        new = set()
+        return [k for k in keys if k not in new and not new.add(k)] if unique else list(keys)
+
+    def Minima(self, tol=None): #XXX: unique?
+        if tol is None: tol=self.tol
+        data = self.archive
+        _min = max if self._inv else min
+        _min = _min(data.itervalues())
+        return dict((k,v) for (k,v) in data.iteritems() if round(v, tol) == round(_min, tol))
+
+    def _summarize(self):
+        from __builtin__ import min as _min
+        #NOTE: len(size) = # of dirs; len(vals) = # unique dirs
+        keys = self.Coordinates()
+        vals = self.Values()
+        mins = self.Minima()
+        name = 'max' if self._inv else 'min'
+        min = max if self._inv else _min
+        # print the minimum and number of times the minimum was found
+        print "%s: %s (count=%s)" % (name, min(mins.values()), len(mins))
+        # print number of minima found, number of unique minima, archive size
+        print "pts: %s (values=%s, size=%s)" % (len(set(keys)), len(set(vals)), len(keys))
+        #_i = max(dict(i).values())
+        return
+
+
+    def Trajectories(self): #XXX: better/alternate, read from evalmon?
+        from mystic.munge import read_trajectories
+        if not self.traj:
+            try: #NOTE: FRAGILE (if absolute path is not used)
+                filename = self.solver._stepmon._filename
+                step, param, cost = read_trajectories(filename)
+            except AttributeError:
+                msg = "a LoggingMonitor or UseTrajectories is required"
+                raise RuntimeError(msg)
+        else:
+            step = []; cost = []; param = [];
+            for sprayer in self._allSolvers:  #XXX: slow? better thread.map?
+                for seeker in sprayer._allSolvers:
+                    values = read_trajectories(seeker._stepmon)
+                    step.extend(values[0])
+                    param.extend(values[1])
+                    cost.extend(values[2])
+        #XXX: (not from archive, so) if self._inv: use -cost
+        return step, param, cost
+
+    def Samples(self):
+        import numpy as np
+        xy,xy,z = self.Trajectories()
+        xy = np.vstack((np.array(xy).T,z))
+        if self._inv: xy[-1,:] = -xy[-1]
+        return xy #NOTE: actually xyz
+
+    pass
+
+
+
+# EOF
