44546990f40464ccbb5a99c00ec722bc6d41b9f7
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2019-01-03 07:03:35 +0000

    limit numpy for 3.4 and pypy
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@1024 968178ea-60bd-409e-af13-df8a517b6005

--- setup.py
+++ setup.py
@@ -379,8 +379,11 @@ if sysversion < (2,6) or (sysversion >= (3,0) and sysversion <= (3,1)):
 elif sysversion == (2,6) or (sysversion >= (3,2) and sysversion <= (3,3)):
     numpy_version = '>=1.0, <1.12.0'
     sympy_version = '>=0.6.7, <1.1'
+elif sysversion == (sysversion >= (3,4) and sysversion < (3,5)):
+    numpy_version = '>=1.0, <1.16.0'
+    sympy_version = '>=0.6.7, <1.1'
 else:
-    numpy_version = '>=1.0'
+    numpy_version = '>=1.0, <1.16.0' #FIXME: pypy bug in numpy
     sympy_version = '>=0.6.7'#, <0.7.4'
 dill_version = '>=0.2.8.2'
 klepto_version = '>=0.1.5.2'
