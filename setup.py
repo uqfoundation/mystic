df1068b948e9304e5fa9a9ae339a4fc9c5479b0b
mmckerns <mmckerns@968178ea-60bd-409e-af13-df8a517b6005>
2019-10-03 00:42:21 +0000

    handle numpy versioning correctly
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/mystic/mystic@1107 968178ea-60bd-409e-af13-df8a517b6005

--- setup.py
+++ setup.py
@@ -374,14 +374,14 @@ setup(name='mystic',
 
 # add dependencies
 sysversion = sys.version_info[:2]
-if sysversion < (2,6) or (sysversion >= (3,0) and sysversion <= (3,1)):
+if sysversion < (2,6) or sysversion == (3,0) or sysversion == (3,1):
     numpy_version = '>=1.0, <1.8.0'
     sympy_version = '>=0.6.7, <1.1'
-elif sysversion == (2,6) or (sysversion >= (3,2) and sysversion <= (3,3)):
+elif sysversion == (2,6) or sysversion == (3,2) or sysversion == (3,3):
     numpy_version = '>=1.0, <1.12.0'
     sympy_version = '>=0.6.7, <1.1'
-elif sysversion == (sysversion >= (3,4) and sysversion < (3,5)):
-    numpy_version = '>=1.0'
+elif sysversion == (2,7) or sysversion == (3,4):
+    numpy_version = '>=1.0, <1.17.0'
     sympy_version = '>=0.6.7, <1.1'
 else:
     numpy_version = '>=1.0'
