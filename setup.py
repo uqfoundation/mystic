5099806d225b685b8612614c9b60a36a40e8d18a
mmckerns <mmckerns@caltech.edu>
2021-06-14 19:27:03 -0400

    tag mystic-0.3.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if unsupported:
     raise ValueError(unsupported)
 
 # set version numbers
-stable_version = '0.3.7'
+stable_version = '0.3.8'
 target_version = '0.3.8'
 is_release = stable_version == target_version
 
