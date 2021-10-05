085e33a48cfff73726148672a99830b2cfc5891a
mmckerns <mmckerns@caltech.edu>
2021-10-05 19:29:59 -0400

    formal support for python 3.10

--- setup.py
+++ setup.py
@@ -376,6 +376,7 @@ setup(name='mystic',
                      'Programming Language :: Python :: 3.7',
                      'Programming Language :: Python :: 3.8',
                      'Programming Language :: Python :: 3.9',
+                     'Programming Language :: Python :: 3.10',
                      'Topic :: Scientific/Engineering',
                      'Topic :: Software Development'],
 
