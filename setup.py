bd183d9c77d647659a4f0c1f252aa87095533322
mmckerns <mmckerns@caltech.edu>
2022-05-19 17:21:12 -0400

    fix LONG_DOC and add project_urls

--- setup.py
+++ setup.py
@@ -37,13 +37,13 @@ try:
     for line in meta_fh:
         if LONG_DOC:
             if line.startswith(DOC_STOP):
-                LONG_DOC += DOC_STOP.rstrip()
+                LONG_DOC = LONG_DOC.strip().strip("'").strip('"').lstrip()
                 break
             else:
                 LONG_DOC += line
         elif line.startswith('__doc__'):
             DOC_STOP = line.split(' = ')[-1]
-            LONG_DOC += DOC_STOP
+            LONG_DOC = "\n"
     meta['LONG_DOC'] = LONG_DOC
 finally:
     meta_fh.close()
@@ -79,21 +79,29 @@ setup_kwds = dict(
     platforms = ['Linux', 'Windows', 'Mac'],
     url = 'https://github.com/uqfoundation/mystic',
     download_url = 'https://pypi.org/project/mystic/#files',
+    project_urls = {
+        'Documentation':'http://mystic.rtfd.io',
+        'Source Code':'https://github.com/uqfoundation/mystic',
+        'Bug Tracker':'https://github.com/uqfoundation/mystic/issues',
+    },
     python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*',
-    classifiers = ['Development Status :: 5 - Production/Stable',
-                   'Intended Audience :: Developers',
-                   'Intended Audience :: Science/Research',
-                   'License :: OSI Approved :: BSD License',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.7',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9',
-                   'Programming Language :: Python :: 3.10',
-                   'Programming Language :: Python :: Implementation :: PyPy',
-                   'Topic :: Scientific/Engineering',
-                   'Topic :: Software Development'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+    ],
     packages = ['mystic','mystic.models','mystic.math','mystic.cache',
                 'mystic.tests'],
     package_dir = {'mystic':'mystic','mystic.models':'models',
