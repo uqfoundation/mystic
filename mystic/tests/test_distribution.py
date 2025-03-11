8a4ffa71d31e961089aa4deb17e8737dac95a6fa
mmckerns <mmckerns@caltech.edu>
2025-03-11 18:51:56 -0400

    enable weighting of joined Distributions

--- mystic/tests/test_distribution.py
+++ mystic/tests/test_distribution.py
@@ -18,9 +18,16 @@ apb2= (a+b)/2
 anb = Distribution(a,b)
 bnc = Distribution(b,c)
 bpc2 = (b+c)/2
+amb = a*b
+a0nb = Distribution(a,b,p=(0,1))
+a4nb6 = Distribution(a,b,p=(.4,.6))
+anc = Distribution(a,c,p=(.5,.5))
 
 assert almostEqual(a2pb2(N).mean(), apb2(N).mean(), tol=.1)
 assert almostEqual(anb(N).mean(), .5*apb(N).mean(), tol=.1)
+assert almostEqual(anb(N).mean(), amb(N).mean(), tol=.1)
 assert almostEqual((a(N).mean() + b(N).mean())/2, apb2(N).mean(), tol=.1)
 assert almostEqual((b(N).mean() + c(N).mean())/2, bpc2(N).mean(), tol=.1)
 assert almostEqual((a+b+c)(N).mean(), (c+b+a)(N).mean(), tol=.1)
+assert almostEqual(a0nb(N).mean(), b(N).mean(), tol=.1)
+assert almostEqual(a4nb6(N).mean(), anc(N).mean(), tol=.1)
